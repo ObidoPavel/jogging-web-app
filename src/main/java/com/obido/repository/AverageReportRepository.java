3b835033301b7d3e2e6fa3752b95a4ab9d988c1c
Pavel_Obido <pobido@edmunds.com>
2019-02-13 11:54:11 +0300

    Spring Data nativeQuery instead of JdbcTemplate

--- src/main/java/com/obido/repository/AverageReportRepository.java
+++ src/main/java/com/obido/repository/AverageReportRepository.java
@@ -1,11 +1,21 @@
 package com.obido.repository;
 
 import com.obido.domain.AverageReportBean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface AverageReportRepository {
+public interface AverageReportRepository extends JpaRepository<AverageReportBean, Long> {
 
-    List<AverageReportBean> retrieveReport(Long userId);
+    @Query(
+            value = "SELECT EXTRACT(WEEK FROM date) AS numberOfWeek, " +
+                    "AVG(distance/duration) AS avgSpeed, " +
+                    "AVG(duration) AS avgDuration, " +
+                    "SUM(distance) AS sumDist FROM JOG " +
+                    "WHERE user_id = :userId GROUP BY numberOfWeek",
+            nativeQuery = true)
+    List<AverageReportBean> retrieveReport(@Param("userId") Long userId);
 
 }
